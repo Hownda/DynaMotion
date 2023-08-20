d2359e88b69f1d41ece98c5210aa527ee34a87db
Hownda <117111277+Hownda@users.noreply.github.com>
2023-08-20 19:11:38 +0200

    Added an update function

--- DynaMotion/DynaMotion/Demo.cs
+++ DynaMotion/DynaMotion/Demo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,23 @@ namespace DynaMotion
     class Demo : PhysicsEngine
     {
         public Demo() : base(new Vector2(615, 515), "Physics Engine Demo") { }
+
+        public override void OnLoad()
+        {
+            Console.WriteLine("Hello World");
+            BackgroundColor = Color.Black;
+        }
+
+        public override void OnDraw()
+        {
+            
+        }        
+
+        int frame = 0;
+        public override void OnUpdate()
+        {
+            Console.WriteLine($"Frame Count: {frame}");
+            frame++;
+        }
     }
 }
--- DynaMotion/DynaMotion/DynaMotion/PhysicsEngine.cs
+++ DynaMotion/DynaMotion/DynaMotion/PhysicsEngine.cs
@@ -21,6 +21,9 @@ namespace DynaMotion.DynaMotion
         public Vector2 ScreenSize = new Vector2(512, 512);
         private string Title = "Physics Engine";
         private Canvas Window = null;
+        private Thread PhysicsLoopThread = null;
+
+        public Color BackgroundColor = Color.Green;
 
         public PhysicsEngine(Vector2 ScreenSize, string Title)
         {
@@ -30,8 +33,41 @@ namespace DynaMotion.DynaMotion
             Window = new Canvas();
             Window.Size = new Size((int)this.ScreenSize.X, (int)this.ScreenSize.Y);
             Window.Text = this.Title;
+            Window.Paint += Renderer;
+
+            PhysicsLoopThread = new Thread(PhysicsLoop);
+            PhysicsLoopThread.Start();
 
             Application.Run(Window);
         }
+
+        void PhysicsLoop()
+        {
+            OnLoad();
+            while(PhysicsLoopThread.IsAlive)
+            {
+                try
+                {
+                    OnDraw();
+                    Window.BeginInvoke((MethodInvoker)delegate { Window.Refresh(); });
+                    OnUpdate();
+                    Thread.Sleep(1);
+                }
+                catch
+                {
+                    Console.WriteLine("Eninge is loading...");
+                }
+            }
+        }
+
+        private void Renderer(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(BackgroundColor);
+        }
+
+        public abstract void OnLoad();
+        public abstract void OnUpdate();
+        public abstract void OnDraw();
     }
 }
