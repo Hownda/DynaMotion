135d3eb7e51ed3966b05502e3de200339f2f2608
Hownda <117111277+Hownda@users.noreply.github.com>
2023-08-20 19:45:46 +0200

    Added a debugger

---
+++ DynaMotion/DynaMotion/DynaMotion/Debug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynaMotion.DynaMotion
+{
+    public class Debug
+    {
+        public static void Log(string logText)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"[Log] - {logText}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void LogWarning(string logText)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[Warning] - {logText}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void LogError(string logText)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[Exception] - {logText}");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
